64e1b026ff747b9a9cafea1a82d259e86f362d86
KB <kevman646@gmail.com>
2024-12-12 11:16:47 -0700

    final book report

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ def main():
     #print (c_words)
     c_char = count_char(book_text)
     #print(c_char)
+    
+    #run book report
+    book_report(book_path,c_words,c_char)
+
+
 
 def read_book(bp):
     with open(bp) as f:
@@ -22,11 +27,19 @@ def count_char(text):
     text_lowered = text.lower()
     result_dic = {}
     for letter in text_lowered:
-        if letter in result_dic:
-            result_dic[letter] += 1
-        else:
-            result_dic[letter] = 1
+        if letter.isalpha():
+            if letter in result_dic:
+                result_dic[letter] += 1
+            else:
+                result_dic[letter] = 1
     #print(result_dic)
     return result_dic
+
+def book_report(file,words,char):
+    print(f"--- Begin report of {file} ---")
+    print(f"{words} words found in the document")
+    for i in char:
+        print(f"The '{i}' character was found {char[i]} times")
+    print("--- End report ---")
 
 main()
