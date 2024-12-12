aedcb366c6e9c9f021ff1a14bea718690c019601
KB <kevman646@gmail.com>
2024-12-12 08:38:57 -0700

    book reader completed

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    book_text = read_book(book_path)
+    print(book_text)
+
+def read_book(bp):
+    with open(bp) as f:
+        file_contents = f.read()
+    return file_contents
+
+main()
